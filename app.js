ea1a5a0233f301b8787dee51f3bebb40759153e0
James McLaughlin <james.mclaughlin2@sky.uk>
2016-12-20 10:02:20 +0000

    Added public, src, test, route files
    Installed bower, ejs, jade

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
-app.get('/index.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+var port = 4000;
 
-app.get('/cameratest.html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/cameratest.html'));
-});
+var formRouter = require('./src/routes/formRoutes');
+// var path = require('path');
 
+app.use(express.static('public'));
+app.set('views','./src/views');
 
+app.set('view engine', 'ejs');
+
+app.use('/userform', formRouter);
 
 app.get('/',function(req,res) {
-    res.send("Hello World");
-
+    res.render('index', {
+        title: 'Hello from render',
+        nav:[{
+            Link: '/',
+            Text: 'Home'
+            },{
+            Link: '/userform',
+            Text: 'User Form'
+            },{
+            Link: '/album',
+            Text: 'Album'
+        }]
+    });
 });
 
 
 
+// app.get('/index.html', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
+//
+// app.get('/cameratest.html', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/cameratest.html'));
+// });
 
-app.listen(4000);
+
+app.listen(port, function(){
+    console.log("Server listening at port 4000");
+});
